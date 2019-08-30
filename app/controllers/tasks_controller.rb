b5f22b0bcd995bf171a46e7e775f8140a9ad1817
tetsusen <kuromayo_0521@yahoo.co.jp>
2019-08-30 13:09:03 +0000

    user login

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,7 +3,8 @@ class TasksController < ApplicationController
     before_action :set_task, only: [:show, :edit, :update, :destroy]
     def index
         if logged_in?
-            @tasks = Task.all
+            @tasks = current_user.tasks.build
+            @tasks = current_user.tasks.order(id: :desc).page(params[:page])
         end
     end
     
@@ -15,12 +16,14 @@ class TasksController < ApplicationController
     end
     
     def create
-        @task = Task.new(task_params)
+        #@task = Task.new(task_params)
+        @task = current_user.tasks.build(task_params)
 
         if @task.save
             flash[:success] = 'タスクが正常に作成されました'
-            redirect_to @task
+            redirect_to root_url
         else
+            @tasks = current_user.tasks.order(id: :desc).page(params[:page])
             flash.now[:danger] = 'タスクが作成されませんでした'
             render :new
         end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,18 +13,6 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
-  def create
-    @user = User.new(user_params)
-
-    if @user.save
-      flash[:success] = 'ユーザを登録しました。'
-      redirect_to @user
-    else
-      flash.now[:danger] = 'ユーザの登録に失敗しました。'
-      render :new
-    end
-  end
-  
   private
 
   def user_params
