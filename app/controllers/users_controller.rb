253b5ee849aa0fcbb06b5894274bf6abaa10cd6f
tetsusen <kuromayo_0521@yahoo.co.jp>
2019-08-30 14:31:50 +0000

    add singup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,6 +12,18 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+  
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      flash[:success] = 'ユーザを登録しました。'
+      redirect_to root_url
+    else
+      flash.now[:danger] = 'ユーザの登録に失敗しました。'
+      render :new
+    end
+  end
 
   private
 
